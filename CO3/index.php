9f361139ef574b0cedb892e045039a43ca5dbd6e
aswin r <119281218+aswinr019@users.noreply.github.com>
2023-01-15 22:43:43 +0530

    Add files via upload

--- CO3/index.php
+++ CO3/index.php
@@ -1,18 +1,19 @@
 
 <?php
 
-if($_SERVER['REQUEST_METHOD'] =='POST'){
+if(isset($_POST['submit'])){
 
 
 $fname = $_POST['fname'];
 $lname = $_POST['lname'];
+$age = $_POST['age'];
 $email = $_POST['email'];
-
+$address = $_POST['address'];
 
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "";
+$database = "new_db";
 
 $connection = mysqli_connect($servername,$username,$password,$database);
 
@@ -21,7 +22,25 @@ if(!$connection){
     die("Failed to connect : ".mysqli_connect_error());
 }
 else {
+
     echo "Connected succesfully";
+    
+    $sql = "INSER INTO student VALUES('$fname','$lname','$age','$email','$address')";
+
+    $result = mysqli_query($connection,$sql);
+
+    if($result){
+        echo "Succesfully inserted";
+
+        $sql = "SELECT * FROM student";
+
+        $result = mysqli_query($connection,$sql);
+
+        
+    }
+    else {
+        echo "Failed to insert row";
+    }
 }
 
 
