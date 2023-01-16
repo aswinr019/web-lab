dd5a6025d1225b3b07f1f37fb04f4778d9528fb2
aswin r <119281218+aswinr019@users.noreply.github.com>
2023-01-16 06:38:31 +0530

    Add files via upload

--- CO3/index.php
+++ CO3/index.php
@@ -36,6 +36,24 @@ else {
 
         $result = mysqli_query($connection,$sql);
 
+        $number = mysqli_num_rows($result);
+
+        echo '<table border"1">';
+        echo '<tr><th>First Name</th><th>Last name</th><th>Age</th><th>Email</th><th>Address</th></tr>';
+        if($number > 0){
+            while( $row = mysqli_fetch_assoc($result)){
+                // echo var_dump($row);
+                // echo '<br>';
+
+                echo '<tr><td>'.$row['fname'].'</td>';
+                echo '<td>'.$row['lname'].'</td>';
+                echo '<td>'.$row['ahe'].'</td>';
+                echo '<td>'.$row['email'].'</td>';
+                echo '<td>'.$row['address'].'</td></tr>';
+
+            }
+        }
+        echo '</table>';
         
     }
     else {
@@ -44,16 +62,7 @@ else {
 }
 
 
-$sql = "INSERT INTO `dbname` (`fname`,`lname`,`email`) VALUES ('$fname','$lname','$email') ";
 
-$result = mysqli_query($connection,$sql);
-
-if($result){
-    echo "Records was inserted succesfully";
-}
-else {
-    echo "Unable to insert records ".mysqli_error($connection);
-}
 
 
 
